11d25c0f34f1b0f3cb5a803af8b9ed7c526c887e
plfmoura <plfmoura96@gmail.com>
2023-01-18 22:57:33 -0300

    alter(form)script and styles

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -36,9 +36,9 @@ export default function Form() {
                 newElement.innerHTML = `
                     <div className="user-msg-container">
                         <div className="msg-content">
-                            <p>Usuário: ${ item.name }</p>
+                            <p style="font-weight:700">Usuário: ${ item.name }</p>
                             <p>Email: ${ item.email }</p>
-                            <p>Mensagem: ${ item.text }</p>
+                            <p style="color:red">Mensagem: ${ item.text }</p>
                         </div>
                     </div>
                     `
@@ -46,7 +46,17 @@ export default function Form() {
             })
         }
 
-        mostraMensagens()
+        if(result[0].name == ''){
+            alert('Digite seu nome de usuário.')
+        } else if(result[0].email == '') {
+            alert('Digite seu email de cadastro.')
+
+        } else if(result[0].text == '') {
+            alert('Digite sua mensagem.')
+        } else {
+            mostraMensagens()
+        }
+
         return result
     }
 
@@ -55,13 +65,13 @@ export default function Form() {
         <form action="" className='form-container'>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" className="name" onChange={ event => setName(event.target.value) }/>
+                <input type="text" className="name" maxLength={20} onChange={ event => setName(event.target.value) }/>
             </div>
             <div>
                 <label htmlFor="email">E-mail:</label>
-                <input type="text" className="email" onChange={ event => setEmail(event.target.value) }/>
+                <input type="email" className="email" onChange={ event => setEmail(event.target.value) }/>
             </div>
-            <textarea name="texto" cols="30" rows="10" className='area-texto' 
+            <textarea name="texto" cols="30" rows="10" maxLength={340} className='area-texto' 
                 onChange={ event => setMsg(event.target.value) }
             />
             <input type="button" value="Enviar" className='enviar' onClick={ getValues }/>
